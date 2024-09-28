7af172e9e0285f050d3a53aab3e3c6b09a6edd2f
Denis Kolenteev <kolenteevdenis@gmail.com>
2024-09-28 19:31:37 +0300

    scraper func

--- scraper.go
+++ scraper.go
@@ -1,5 +1,59 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Product struct {
+	name, price string
+}
+
+func scraped() ([]Product, error) {
+
+	var products []Product
+
+	res, err := http.Get("https://www.scrapingcourse.com/ecommerce/")
+	if err != nil {
+		return products, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return products, err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return products, err
+	}
+
+	doc.Find("li.product").Each(func(i int, p *goquery.Selection) {
+
+		product := Product{} 
+
+		product.name = p.Find("h2").Text()
+		product.price = p.Find("span.price").Text()
+
+		products = append(products, product)
+
+	})
+
+	return products, nil
+}
+
+func main() {
+
+	result, err := scraped()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, val := range result {
+
+		fmt.Println(val)
+	}
 }
